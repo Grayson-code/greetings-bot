04bc387a9c2a8944cedad1702e76dc06a236ba13
Grayson-code <northernstarmusic10@gmail.com>
2022-06-04 19:25:35 +0530

    fix bug

--- dist/listeners/ws.js
+++ dist/listeners/ws.js
@@ -22,7 +22,7 @@ let UserEvent = class UserEvent extends framework_1.Listener {
         const app = (0, express_1.default)();
         const server = http_1.default.createServer(app);
         const io = new socket_io_1.Server(server);
-        server.listen(3000, () => {
+        server.listen(process.env.PORT || 3000, () => {
             console.log(chalk_1.default.green('[WS] Listening on port 3000'));
         });
         this.container.logger.info('Socket.io is running');
