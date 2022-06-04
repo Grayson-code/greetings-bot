2e17ac023a8b4acb9c50ed2728373e7ecf9bc526
Grayson-code <northernstarmusic10@gmail.com>
2022-06-04 19:16:14 +0530

    aaaa

--- test.js
+++ test.js
@@ -3,12 +3,12 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const { Message } = require("discord.js");
+// const { Message } = require("discord.js");
 
-// const array = ['hi', '3', '3', '4', '5' , '6', '7', '8', '9', '10'];
-// const largerArray = array.map(value => String(Number(value ) + 1));
-// console.log(largerArray);
+// // const array = ['hi', '3', '3', '4', '5' , '6', '7', '8', '9', '10'];
+// // const largerArray = array.map(value => String(Number(value ) + 1));
+// // console.log(largerArray);
 
-const test = (message: Message) => {
-    message.guild.members.cache.filter
-}
+// const test = (message: Message) => {
+//     message.guild.members.cache.filter
+// }
